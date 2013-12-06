f97ee8bbd5d9dea4211112b631e89f61ec234c2b
SharangP <sharang.phadke@gmail.com>
2013-12-06 16:18:27 -0500

    moved node class inside neural net

--- neuralnet.rb
+++ neuralnet.rb
@@ -1,15 +1,34 @@
 #########################
-# Neural Net
+# Neural Network
 #
 # Sharang Phadke
 # 11/30/2013
 #########################
 
-require_relative 'node'
-
 
 # Neural Network class
 class NeuralNet
+
+    # Node class
+    class Node
+        @@bias_input = -1
+        
+        def self.bias_input
+            @@bias_input
+        end
+
+        def initialize()
+            @inputs = []
+            @weights = []
+            @bias_weight = nil
+            @inval = nil
+            @activation = nil
+            @delta = nil
+        end
+
+        attr_accessor :inputs, :weights, :bias_weight, :inval, :activation, :delta
+    end
+
 
     # Constructor
     def initialize()
